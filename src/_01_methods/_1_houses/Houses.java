b419cbb0f7844123a3bb8982e8f5506d2e3cb3f4
Gavin <gzhu100@icloud.com>
2022-12-10 11:26:35 -0800

    started on Houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,19 +1,20 @@
 package _01_methods._1_houses;
+
+import java.awt.*;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
+
 	public static void main(String[] args) {
-		Houses house = new Houses();
 
 	}
 
 		public void run () {
 			// Check the recipe to find out what code to put here
-			Robot rob = new robot();
-			rob.setX(0);
-			rob.setY(500);
+			Robot rob = new Robot();
 		}
 	}
 
