429ceb5a6ebd3af37078c51d04b6b861d85d6baa
Gavin <gzhu100@icloud.com>
2022-12-24 11:27:03 -0800

    finihed FinishLine, Houses, and working on list of goals

--- src/_01_methods/_1_houses/Houses_2.java
+++ src/_01_methods/_1_houses/Houses_2.java
@@ -3,6 +3,7 @@ package _01_methods._1_houses;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.*;
+import java.util.Random;
 
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
@@ -10,6 +11,7 @@ import java.awt.*;
  */
 public class Houses_2 {
     private Robot rob = new Robot();
+    Random ran = new Random();
 
     public static void main(String[] args) {
         Houses_2 houses = new Houses_2();
@@ -37,11 +39,13 @@ public class Houses_2 {
     }
 
     public void run() {
-        for (int i = 0; i < 10; i++) {
-			drawFlatTop(getRandomHeight(i));
+        for (int i = 0; i < 4; i++) {
+            drawFlatTop(getRandomHeight(i));
+
+            drawFlatTop("small");
+            drawFlatTop(Color.MAGENTA, "medium");
+            drawPointyRoof(getRandomHeight(ran.nextInt(10)+1), Color.YELLOW);
         }
-		drawFlatTop("small");
-		drawFlatTop(Color.MAGENTA, "medium");
     }
 
     // Step 1
@@ -61,33 +65,51 @@ public class Houses_2 {
         rob.setPenColor(0, 255, 0);
         rob.move(25);
         rob.turn(-90);
-		rob.setPenColor(0,0,0);
+        rob.setPenColor(0, 0, 0);
     }
-	//step 5
-	public void drawFlatTop(String heightS){
-		int heightValue = 0;
-		if (heightS.equalsIgnoreCase("small")){
-			heightValue =60;
-		}
-		else if (heightS.equalsIgnoreCase("medium")){
-			heightValue =120;
-		}
-		else if (heightS.equalsIgnoreCase("large")){
-			heightValue =250;
-		}
-		drawFlatTop(heightValue);
-	}
-	//step 6
-	public void drawFlatTop(Color color, String height){
-		rob.setPenColor(color);
-		drawFlatTop(height);
-	}
-	//part of step 4
-	public int getRandomHeight(int forLoopIndex){
-		int height = 0;
-		height = forLoopIndex*30;
-		return height;
-	}
+
+    //step 5
+    public void drawFlatTop(String heightS) {
+        int heightValue = 0;
+        if (heightS.equalsIgnoreCase("small")) {
+            heightValue = 60;
+        } else if (heightS.equalsIgnoreCase("medium")) {
+            heightValue = 120;
+        } else if (heightS.equalsIgnoreCase("large")) {
+            heightValue = 250;
+        }
+        drawFlatTop(heightValue);
+    }
+
+    //step 6
+    public void drawFlatTop(Color color, String height) {
+        rob.setPenColor(color);
+        drawFlatTop(height);
+    }
+
+    //step 7
+    public void drawPointyRoof(int height, Color color) {
+        rob.setPenColor(color);
+        rob.move(height);
+        rob.turn(45);
+        rob.move(height / 10);
+        rob.turn(90);
+        rob.move(height / 10);
+        rob.turn(45);
+        rob.move(height);
+        rob.turn(-90);
+        rob.setPenColor(0, 255, 0);
+        rob.move(25);
+        rob.turn(-90);
+    }
+
+    //part of step 4
+    public int getRandomHeight(int forLoopIndex) {
+        int height = 0;
+        height = forLoopIndex * 30;
+        return height;
+    }
+
     public void hou(String height, int r, int g, int b) {
         int size = 60;
         if (height.equalsIgnoreCase("medium")) {
