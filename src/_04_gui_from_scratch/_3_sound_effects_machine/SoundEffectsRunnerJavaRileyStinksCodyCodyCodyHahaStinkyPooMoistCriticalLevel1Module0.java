4d8b7cb37e1a10e17c5a27bc7118bbddfab0d635
league <league@imac-14.attlocal.net>
2023-01-22 11:24:55 -0800

    uyfdsg pugfbdpugsewsa hlzjxvå¬iasxvcc pq23io;bjp9uß´†∂®¥†ƒ¨¥©¨ˆ˙ø∆πø•ø¶®ˆ§•ƒ†¥©∫¨ø∆πø¥ø©ƒ†¨√˙º¨ª•ø¥©ƒç¥¨©˙ªˆ¥†¨∂¥˙ø¥¨†∂¨ƒ¥©ˆπˆ§´¨∞∂®ç¥©¨ˆºøª¨¥¶ˆ©ƒƒƒƒƒ©˙∆˙©ƒπøˆ¨©ƒ˚˙©ƒ∂–“π…¬˚∆˙´∞§¶•ªº–≠“‘…¬˚∆˙©ƒ∂®†¥¨ˆøπß∂ƒ©˙∆˚¬ ≤˙¨∆µ¨∆∂®çˆ˚ˆƒ®

--- src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsRunnerJavaRileyStinksCodyCodyCodyHahaStinkyPooMoistCriticalLevel1Module0.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsRunnerJavaRileyStinksCodyCodyCodyHahaStinkyPooMoistCriticalLevel1Module0.java
@@ -4,6 +4,6 @@ import _04_gui_from_scratch._2_jack_in_the_box.JackInTheBox;
 
 public class SoundEffectsRunnerJavaRileyStinksCodyCodyCodyHahaStinkyPooMoistCriticalLevel1Module0 {
 	 public static void main(String[] args) {
-	        new JackInTheBox().showButton();
+	        new soundEffects().showButton();
 	    }
 }
--- src/_04_gui_from_scratch/_3_sound_effects_machine/soundEffects.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/soundEffects.java
@@ -1,7 +1,62 @@
 package _04_gui_from_scratch._3_sound_effects_machine;
 
-public class soundEffects {
-public void showButton(){
-	
-}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class soundEffects implements ActionListener{
+		JButton sound1 = new JButton("sound1");
+	    JButton sound2 = new JButton("sound2");
+	    JButton sound3 = new JButton("sound3");
+	    public void showButton(){
+	        JFrame frame = new JFrame();
+	        JPanel panel = new JPanel();
+	        panel.add(sound1);
+	        panel.add(sound2);
+	        panel.add(sound3);
+	        sound1.addActionListener(this);
+	        sound2.addActionListener(this);
+	        sound3.addActionListener(this);
+	        frame.add(panel);
+	        frame.setVisible(true);
+	        frame.pack();
+	        
+	}
+	    String vineboom = "sawing-wood-daniel_simon.wav";
+	    private void playSound(String soundFile) {
+		String path ="src/_04_gui_from_scratch/_3_sound_effects_machine/";
+				File	sound = new File(path+soundFile);
+				if (sound.exists()) {
+					new Thread(() -> {
+					try {
+						Clip clip = AudioSystem.getClip();
+						clip.open(AudioSystem.getAudioInputStream(sound));
+						clip.start();
+						Thread.sleep(clip.getMicrosecondLength()/1000);
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+						System.out.println("Could not play this sound");
+					}}).start();
+		 		}
+				else {
+					System.out.println("File does not exist");
+					System.out.println("Working Directory = " + System.getProperty("user.dir"));
+					System.out.println(sound);
+				}
+			
+		}
+	    @Override
+		public void actionPerformed(ActionEvent e) {
+	    	if (e.getSource() == sound1) {
+	            playSound(vineboom);
+	        }
+	    	System.out.println("test");
+		}
 }
